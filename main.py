1ce24c76591a23ca77ccacfdbb62ece93786ea14
alinalin10 <152095576+alinalin10@users.noreply.github.com>
2024-06-06 03:27:40 +0000

    changed to tradingview.com because it has "+" and "-" signs

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import numpy as np
 
 print("working")
 
-base_url = "https://www.coingecko.com/"
+#base_url = "https://www.coingecko.com/"
+base_url = "https://www.tradingview.com/markets/cryptocurrencies/prices-all/"
 
 tables = []
+test = []
 
 for i in range(1, 4):
     print(f"processing page number {i}")
@@ -16,7 +18,9 @@ for i in range(1, 4):
     }
     response = requests.get(base_url,headers={"User-Agent":"Mozilla/5.0"}, params=params)
     soup = BeautifulSoup(response.content, 'html.parser')
+
     tables.append(pd.read_html(str(soup))[0])
+    break
 
 table_1 = pd.concat(tables)
 table_1 = table_1.loc[:, table_1.columns[1:-1]]
