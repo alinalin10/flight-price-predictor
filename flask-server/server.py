c6f5a40829ca15f8c19f00d4c00e99dd51602b9a
alinalin10 <152095576+alinalin10@users.noreply.github.com>
2024-06-09 00:19:29 +0000

    converted jupyter notebook to python script

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,5 +1,19 @@
 from flask import Flask
+import pandas as pd
+import function
+from sklearn.model_selection import train_test_split
+import pickle
 
+
+
+# loading model
+loaded_model = pickle.load(open('/workspaces/new-project/model_0.sav', 'rb'))
+
+# try out
+data = function.try_data(0, 0, 2.17, 22, 'Indigo', 'Late_Night', 'Early_Morning', 'Delhi', 'Mumbai')
+print(loaded_model.predict(data))
+
+# app
 app = Flask(__name__)
 
 @app.route("/members")
@@ -8,3 +22,5 @@ def members():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
